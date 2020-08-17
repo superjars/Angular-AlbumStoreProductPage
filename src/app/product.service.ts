24f2906701b90f425d2729586ebcb0652a3568f6
Jared Bearss <Jared.D.Bearss@EMCIns.com>
2020-08-17 09:22:24 -0500

    Completed Part 3 Module

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ProductService {
+  private _albumUrl: string = '../assets/album.json'
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): (Observable<Album>) {
+    return this._http.get(this._albumUrl).map((response: Response) => {
+      response.json();
+    });
+  }
+}
