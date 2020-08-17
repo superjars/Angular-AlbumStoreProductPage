984f138481f4a7b4cfbcd7914e3f769b6d4615f2
Jared Bearss <Jared.D.Bearss@EMCIns.com>
2020-08-17 09:25:42 -0500

    Completed Part 3 Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,9 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): (Observable<Album>) {
-    return this._http.get(this._albumUrl).map((response: Response) => {
-      response.json();
-    });
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
